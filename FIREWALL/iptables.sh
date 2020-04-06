38274486ff84a0cc82129959dc888ba0c1c56ac4
SofiaFernandez <sfernandez@loycus.com>
2020-04-06 12:44:11 +0200

    * Include gitignore in project * Include README.md * Include Suricata Folder * First interpretation of the arquitecture * Include docker-compose files * Create LICENSE * Create CODE_OF_CONDUCT.md

---
+++ FIREWALL/iptables.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+declare -a TCP_PORTS=22
+declare -a HOSTS=192.168.2.0/24
+# readonly eth="br-$(docker network ls | grep tfm_ddos_demo_tfm | awk '{print $1}')"
+
+iptables -P INPUT ACCEPT
+iptables -F
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+ 
+# Allow incoming ssh only
+for port in ${TCP_PORTS//,/ }; do
+  echo Allowing traffic to TCP $port
+  iptables -A INPUT -p tcp -s 0/0 --dport ${port} -m state --state NEW,ESTABLISHED -j ACCEPT
+done
+
+for host in ${HOSTS//,/ }; do
+  echo Firewall rules docker
+  iptables -I DOCKER-USER -i eth0 ! -s 127.0.0.1 -j DROP
+  # iptables -I DOCKER-USER -i ${INTERFACE} ! -d 192.168.2.0/24 -j DROP 
+  # iptables -I DOCKER-USER -i ${INTERFACE} -p tcp --sport 443 -j ACCEPT 
+  # iptables -I DOCKER-USER -i ${INTERFACE} -p udp --dport 53 -j ACCEPT 
+  echo Block invalid packets
+  iptables -t mangle -A PREROUTING -m conntrack --ctstate INVALID -j DROP
+  echo Block new packets != SYN
+  iptables -t mangle -A PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW -j DROP
+  echo Block MSS values
+  iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
+  echo Block error flags
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP 
+  iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+  echo Block private subnet
+  iptables -t mangle -A PREROUTING -s 224.0.0.0/3 -j DROP 
+  iptables -t mangle -A PREROUTING -s 169.254.0.0/16 -j DROP 
+  iptables -t mangle -A PREROUTING -s 172.16.0.0/12 -j DROP 
+  iptables -t mangle -A PREROUTING -s 192.0.2.0/24 -j DROP 
+  iptables -t mangle -A PREROUTING -s 192.168.0.0/16 -j DROP 
+  iptables -t mangle -A PREROUTING -s 10.0.0.0/8 -j DROP 
+  iptables -t mangle -A PREROUTING -s 0.0.0.0/8 -j DROP 
+  iptables -t mangle -A PREROUTING -s 240.0.0.0/5 -j DROP 
+  iptables -t mangle -A PREROUTING -s 127.0.0.0/8 ! -i lo -j DROP
+  echo Block ICMP traffic
+  iptables -t mangle -A PREROUTING -p icmp -j DROP
+  echo Block taffic by size
+  iptables -A INPUT -p tcp -m connlimit --connlimit-above 85 -j REJECT --reject-with tcp-reset
+  echo Block taffic by size/time
+  iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 50/s --limit-burst 18 -j ACCEPT 
+  iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP 
+  echo Block fragment packets
+  iptables -t mangle -A PREROUTING -f -j DROP
+  echo Block TCP RST
+  iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 4/s --limit-burst 4 -j ACCEPT 
+  iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+  echo Allowing traffic from ${host}
+  iptables -A INPUT -p tcp -s ${host} -m state --state NEW,ESTABLISHED -j ACCEPT
+  echo Filter traffic from ${host} only access Elastic from Logstash and Kibana
+  iptables -I INPUT 1 -p tcp --dport 9200 -j DROP
+  iptables -I INPUT 1 -p tcp --dport 9200 -s ${host} -j ACCEPT
+  iptables -I INPUT 1 -p tcp --dport 9200 -s ${host} -j ACCEPT
+  echo Filter traffic from ${host} logs to process
+  iptables -I INPUT 1 -p tcp --dport 5044 -j DROP
+  iptables -I INPUT 1 -p tcp --dport 5044 -s ${host} -j ACCEPT
+  echo Mitigate scan ports
+  iptables -N port-scan 
+  iptables -A port-scan -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 3 -j RETURN 
+  iptables -A port-scan -j DROP
+done
+
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+
+
+exec syslogd -n -O -
---
+++ README.md
@@ -0,0 +1,24 @@
+# TFM DDoS Attacks
+SME business continuity project applying resilience to DDoS UDP Flood attacks (new version to a different attack) with docker containers
+## Contribute
+
+If you like this project and want to support it, you can contribute to the project 
+
+You can ask any question, suggestion or complaint at the project's reach out to us at the next place:
+- E-Mail: **tfmddos@gmail.com**
+
+Thanks to all project's!
+## License
+
+The project is licensed under the [GNU GPLv3]. This license lets you use the source for free or
+commercial purposes as long as you provide attribution and don’t hold any project member liable.
+
+## Authors
+[<img src="https://media-exp1.licdn.com/dms/image/C4D03AQFIHPM0YOAWAA/profile-displayphoto-shrink_100_100/0?e=1601510400&v=beta&t=4mjK1CXjX10nTWdsDoXHRXg22_oNYXEhgB3oQ3lgWRU" width="100px;"/><br /><sub><b>Sofía Fernández Moreno</b></sub>](http://sofiafernandezmoreno.github.io/)<br /> 
+
+[<img src="https://media-exp1.licdn.com/dms/image/C5603AQHlt9i9HTaD1A/profile-displayphoto-shrink_200_200/0?e=1601510400&v=beta&t=W5X2m2LzLjOG6ty83AqMA5VLFsJ1FaVJT9ha5B8xDa4" width="100px;"/><br /><sub><b>Álvaro Monforte de la Huerga</b></sub>]()
+
+
+[<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"/><br /><sub><b>Vicente Gaitán Garrido</b></sub>]()<br /> 
+
+[GNU GPLv3]: LICENSE
