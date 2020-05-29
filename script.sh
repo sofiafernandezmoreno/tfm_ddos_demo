39fcc709e5ab2a5fa7686b2a8edd4fd7ddd857ea
SofiaFernandez <sfernandez@loycus.com>
2020-05-29 17:05:48 +0200

    1 cluster with waf

--- script.sh
+++ script.sh
@@ -1,1 +1,15 @@
+#!/bin/bash -eu
+
+set -eux -o pipefail -o errexit
+
+
+readonly task=${1}
+if [ "$task" == "up" ] ;then
 docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml up -d
+fi
+if [ "$task" == "stop" ];then
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml stop
+fi
+if [ "$task" == "down" ] ;then
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml down -v
+fi
