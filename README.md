1534a83d9f1da17d561e689a727b9bbdcbdf997c
SofiaFernandez <sfernandez@loycus.com>
2020-05-31 18:56:20 +0200

    Include ip in siem

--- README.md
+++ README.md
@@ -25,3 +25,15 @@ curl -XPOST -D- 'http://192.168.2.76:5601/api/saved_objects/index-pattern' \
     -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}' \
 -u 'tfmddos:tfmdd0s2020'
 ```
+
+### IPTABLES
+TCP_PORTS: A list of TCP Ports which we should accept all traffic to
+HOSTS: A list of hosts for which we should accept all traffic
+
+any other traffic is DROPped.
+
+example usage:
+
+```
+$ docker run --name firewall -e TCP_PORTS=22 -e HOSTS=172.12.1.1/32 --rm -ti --cap-add=NET_ADMIN iptables
+```
--- script.sh
+++ script.sh
@@ -6,40 +6,40 @@ set -eux -o pipefail -o errexit
 readonly task=${1}
 
 if [ "$task" == "up" ] ;then
-docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml up -d
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml -f docker-compose.iptables.yml up -d
 fi
 if [ "$task" == "stop" ];then
-docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml stop
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml -f docker-compose.iptables.yml stop
 fi
 if [ "$task" == "down" ] ;then
-docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml down -v
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml -f docker-compose.iptables.yml down -v
 fi
 
 if [ "$task" == "ps" ] ;then
-docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml ps
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml -f docker-compose.iptables.yml ps
 fi
 
 if [ "$task" == "build" ] ;then
-docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml build
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml -f docker-compose.iptables.yml build
 fi
 
 if [ "$task" == "logsc" ] ;then
 readonly container=${2}
-docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml logs -f $container
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml -f docker-compose.iptables.yml logs -f $container
 fi
 if [ "$task" == "logs" ] ;then
 
-docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml logs -f 
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml -f docker-compose.iptables.yml logs -f 
 fi
 
 if [ "$task" == "exec" ] ;then
 readonly container=${2}
-docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml exec $container /bin/bash
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml -f docker-compose.iptables.yml exec $container /bin/bash
 fi
 
 
 if [ "$task" == "restart" ] ;then
 readonly container=${2}
-docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml restart $container 
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml -f docker-compose.iptables.yml restart $container 
 fi
 
