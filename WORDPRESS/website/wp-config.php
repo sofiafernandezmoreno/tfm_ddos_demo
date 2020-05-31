145c95819777c8fcc51581f47c6ffb414276d984
SofiaFernandez <sfernandez@loycus.com>
2020-05-31 22:35:21 +0200

    Include ip in siem

--- WORDPRESS/website/wp-config.php
+++ WORDPRESS/website/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wordpress');
 define( 'DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
-define( 'DB_HOST', 'webfortress.db:3306');
+define( 'DB_HOST', 'wordpress_db:3306');
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8');
