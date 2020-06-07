0ce2f16f666c35b62ca2050d70e3c0f9e3e91ef5
SofiaFernandez <sfernandez@loycus.com>
2020-06-07 13:06:37 +0200

    Automate script

--- script.sh
+++ script.sh
@@ -4,12 +4,16 @@ set -eux -o pipefail -o errexit
 
 
 readonly task=${1}
-
-if [ "$task" == "up" ] ;then
+if [ "$task" == "attack" ] ;then
 export INTERFACE="br-$(docker network ls | grep tfm_ddos_demo_tfm | awk '{print $1}')"
 docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml -f docker-compose.iptables.yml -f docker-compose.wordpress.yml up -d
 sleep 5
 python ATTACKS/botnet/attack.py
+fi
+if [ "$task" == "up" ] ;then
+export INTERFACE="br-$(docker network ls | grep tfm_ddos_demo_tfm | awk '{print $1}')"
+docker-compose -f docker-compose.siem.yml -f docker-compose.waf.yml -f docker-compose.iptables.yml -f docker-compose.wordpress.yml up -d
+
 fi
 if [ "$task" == "stop" ];then
 export INTERFACE="br-$(docker network ls | grep tfm_ddos_demo_tfm | awk '{print $1}')"
